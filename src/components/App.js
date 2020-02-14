3ed1a1fb07d25d23d123caef57a4f35592ba6b34
Darko Kotoski <dak@prooffice.dk>
2020-02-14 12:06:22 +0100

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = { displayBio: false };
+
+  render() {
+    return <div>React app</div>;
+  }
+}
+
+export default App;
